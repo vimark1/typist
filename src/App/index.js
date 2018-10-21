a55501b159aa7e23b5156e7b6aea2f1be9dd1a01
Muhammad Kamran <smkamranqadri@yahoo.com>
2018-10-22 02:05:41 +0500

    firebase auth implemented.

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import sampleSize from 'lodash.samplesize';
+import cx from 'classnames';
+import firebase from 'firebase';
+import 'firebase/auth';
 
 import Text from '../Text';
 import WordsPerMinute from '../WordsPerMinute';
 import TypingLocation from '../TypingLocation';
+import Signup from '../Signup';
+import Signin from '../Signin';
 
 import words from '../data/words';
+
+import './style.css';
 
 export default class App extends Component {
   state = {
@@ -13,7 +20,7 @@ export default class App extends Component {
     stats: {
       keys: [],
       success: [],
-      fails: [],
+      fails: []
     },
     text: '',
     index: 0,
@@ -21,6 +28,7 @@ export default class App extends Component {
     typed: '',
     start: new Date(),
     wpm: [],
+    activePage: 'app'
   };
 
   componentDidMount() {
@@ -35,55 +43,53 @@ export default class App extends Component {
   }
 
   keyPressHandler = e => {
-    const charCode = (typeof e.which === 'number') ? e.which : e.keyCode;
+    const charCode = typeof e.which === 'number' ? e.which : e.keyCode;
     const char = String.fromCharCode(charCode);
     this.register(char);
-  }
+  };
 
   keyDownHandler = e => {
-    const charCode = (typeof e.which === 'number') ? e.which : e.keyCode;
-    
-    if(charCode === 8) {
+    const charCode = typeof e.which === 'number' ? e.which : e.keyCode;
+
+    if (charCode === 8) {
       this.backspace();
     }
-    
-    if(charCode === 27) {
+
+    if (charCode === 27) {
       this.completed();
     }
-  }
+  };
 
   generateText = () => {
     const { size } = this.state;
 
     const meaningfulWords = words.filter(word => word.length >= 3);
     const newText = sampleSize(meaningfulWords, size).join(' ');
-    const newLetters = newText
-      .split('')
-      .map(letter => ({
-        letter : letter,
-        done : false
-      }));
+    const newLetters = newText.split('').map(letter => ({
+      letter: letter,
+      done: false
+    }));
 
     this.setState({
       text: newText,
-      letters: newLetters,
-    });
-  }
-
-  backspace = () => {
-    const { index } = this.index;
-    if(index === 0) return;
-    this.setState({
-      index: index - 1,
+      letters: newLetters
     });
   };
-  
+
+  backspace = () => {
+    const { index } = this.state;
+    if (index === 0) return;
+    this.setState({
+      index: index - 1
+    });
+  };
+
   completed = function() {
     this.generateText();
     this.setState({
       index: 0,
       typed: '',
-      start: new Date(),
+      start: new Date()
     });
   };
 
@@ -99,7 +105,7 @@ export default class App extends Component {
   register = char => {
     const stat = {
       key: char,
-      ts: +new Date(),
+      ts: +new Date()
     };
 
     const { stats, text, index, letters, typed } = this.state;
@@ -108,57 +114,126 @@ export default class App extends Component {
     const stateUpdate = {
       stats: {
         ...stats,
-        keys: stats.keys.concat(stat),
-      },
-    }
+        keys: stats.keys.concat(stat)
+      }
+    };
 
     if (char !== charAtIndex) {
       stateUpdate.stats = {
         ...stats,
         ...stateUpdate.stats,
-        fails: stats.fails.concat(stat),
+        fails: stats.fails.concat(stat)
       };
     } else {
-      const updatedLetters = letters.map((letter, idx) => (
-        idx === index ?
-          {
-            ...letter,
-            done: true,
-          } :
-          letter
-      ));
+      const updatedLetters = letters.map(
+        (letter, idx) =>
+          idx === index
+            ? {
+                ...letter,
+                done: true
+              }
+            : letter
+      );
 
-      
       stateUpdate.letters = updatedLetters;
       stateUpdate.index = index + 1;
       stateUpdate.typed = `${typed}${char}`;
       stateUpdate.stats = {
         ...stats,
         ...stateUpdate.stats,
-        success: stats.success.concat(stat),
+        success: stats.success.concat(stat)
       };
 
-      let isCompleted = false
+      let isCompleted = false;
 
       if (index === text.length - 1) {
         const { start, wpm } = this.state;
         const calc = this.calcTime(start, new Date());
-        
+
         stateUpdate.wpm = wpm.concat(calc);
         isCompleted = true;
       }
 
-      this.setState(stateUpdate, () => (isCompleted && this.completed()));
+      this.setState(stateUpdate, () => isCompleted && this.completed());
     }
+  };
+
+  changeActivePage(page) {
+    this.setState({ activePage: page });
+  }
+
+  signupSuccess() {
+    this.changeActivePage('app');
+  }
+
+  signinSuccess() {
+    this.changeActivePage('app');
+  }
+
+  signout() {
+    firebase.auth().signOut();
   }
 
   render() {
+    const { user } = this.props;
     const { letters, index, wpm } = this.state;
+    const MenuItem = ({ title, keyword, active, left, onPress }) => (
+      <li
+        className={cx('menu-item', { active: active === keyword, left: left })}
+        onClick={() => this.changeActivePage(keyword)}
+      >
+        {title}
+      </li>
+    );
     return (
-      <div className="App">
-        <Text letters={letters} index={index} />
-        <WordsPerMinute wordsPerMinute={wpm} />
-        <TypingLocation location={index} />
+      <div>
+        <div className={cx('navbar')}>
+          <ul className={cx('menu')}>
+            <MenuItem
+              title="Typist"
+              keyword="app"
+              left
+              active={this.state.activePage}
+            />
+            {!user.uid && (
+              <MenuItem
+                title="Signup"
+                keyword="signup"
+                active={this.state.activePage}
+              />
+            )}
+            {!user.uid && (
+              <MenuItem
+                title="Signin"
+                keyword="signin"
+                active={this.state.activePage}
+              />
+            )}
+            {user.uid && (
+              <li className={cx('menu-item')} onClick={() => this.signout()}>
+                Logout
+              </li>
+            )}
+            {user.uid && (
+              <li className={cx('menu-item')} onClick={() => {}}>
+                {user.displayName || user.email}
+              </li>
+            )}
+          </ul>
+        </div>
+        {this.state.activePage === 'signup' && (
+          <Signup signupSuccess={() => this.signupSuccess()} />
+        )}
+        {this.state.activePage === 'signin' && (
+          <Signin signinSuccess={() => this.signinSuccess()} />
+        )}
+        {this.state.activePage === 'app' && (
+          <div className="App">
+            <Text letters={letters} index={index} />
+            <WordsPerMinute wordsPerMinute={wpm} />
+            <TypingLocation location={index} />
+          </div>
+        )}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebaseCred from './firebase-cred.json';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var config = firebaseCred;
+
+firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(user => {
+  ReactDOM.render(<App user={user || {}} />, document.getElementById('root'));
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
