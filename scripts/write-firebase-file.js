e4b5ffc801a8222186418c2ec528f5f32c92deca
vimark1 <44329742+vimark1@users.noreply.github.com>
2018-10-26 20:54:36 +1300

    update script

--- scripts/write-firebase-file.js
+++ scripts/write-firebase-file.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const { BRANCH } = process.env;
 
 if (!BRANCH) {
@@ -9,5 +10,7 @@ if (!BRANCH) {
 const config = process.env[`FIREBASE_JSON_${BRANCH}`] || process.env.FIREBASE_JSON_DEFAULT;
 
 console.log(`Writting file for ${BRANCH}`);
-fs.writeFileSync('../src/firebase-cred.json', config);
+console.log({ config });
+
+fs.writeFileSync(path.resolve(__dirname, '../src/firebase-cred.json'), config);
 
