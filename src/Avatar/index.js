60ae6038b781455bb206fa60e563c3d6f6082159
vimark1 <vimark1@users.noreply.github.com>
2018-10-28 20:15:08 +1300

    header

--- src/Avatar/index.js
+++ src/Avatar/index.js
@@ -1,31 +1,18 @@
-import React, { Component } from 'react';
-import { default as ReactAvatar } from 'react-avatar';
+import React from 'react';
+import Avatar from 'react-avatar';
 
-export default class Avatar extends Component {
+export default function({ user, size = 40, round = true}) {
 
-  defaultSize = 40;
-
-  get size() {
-    const { size } = this.props;
-    return size || this.defaultSize;
-  }
-
-  get round() {
-    const { round } = this.props;
-    return typeof round !== 'undefined' ? round : true;
-  }
-
-  get googleId() {
-    const { user } = this.props;
+  const getGoogleId = () => {
     const googleData = user.providerData.find(item => item.providerId === 'google.com') || {};
     return googleData.uid;
   }
 
-  render() {
-    const { user } = this.props;
-    return (
-      <ReactAvatar name={user.displayName} email={user.email} googleId={this.googleId} round={this.round} size={this.size} />
-    );
-  }
+  const googleId = getGoogleId();
+
+  return (
+    <Avatar googleId={googleId} name={user.displayName} email={user.email} round={round} size={size} />
+  );
+
 }
 
