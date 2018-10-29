a5899828f00cce9ec611f736069b886a7ecdb43f
Clay Jensen-Reimann <clayreimann@gmail.com>
2018-10-28 21:11:23 -0500

    Refactor Header
    Based on the usage it would appear that `NavLink`s rather than `Link`s were intended.

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {NavLink} from 'react-router-dom';
 import cx from 'classnames';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
 import Avatar from '../Avatar';
+
+const MenuItem = ({children, path, title, onClick, display}) => display
+  ? (<NavLink className="menu-item" to={path} onClick={onClick} title={title}>
+    {children}
+  </NavLink>)
+  : null;
 
 export default class Header extends Component {
 
@@ -13,49 +19,25 @@ export default class Header extends Component {
   }
 
   render() {
-    const { user } = this.props;
-    const MenuItem = ({ title, path }) => (
-      <li className={cx('menu-item')}>
-        <Link activeClassName='active' to={ path }>
-          {title}
-        </Link>
-      </li>
-    );
-    return (
-      <div className="header">
-        <div className="logo">
-          <h1> <Link to='/'>Typist</Link> </h1>
-        </div>
-        <div className="menu options">
-          <Link className="menu-item" to="/scoreboard">Scoreboard</Link>
-        </div>
-        <ul className={cx('menu')}>
-          {!user.uid && (
-            <MenuItem
-            title="Sign up"
-            path="signup"
-            />
-          )}
-          {!user.uid && (
-            <MenuItem
-            title="Sign In"
-            path="signin"
-            />
-          )}
-          {user.uid && (
-            <li className={cx('menu-item')} onClick={() => this.signout()}>
-            Logout
-            </li>
-          )}
-          {user.uid && (
-            <li className={cx('menu-item')} title={user.displayName || user.email}>
-              <Link to='/profile'>
-                <Avatar user={user} round={true} size="40" />
-              </Link>
-            </li>
-          )}
-        </ul>
+    const {user} = this.props;
+    const loggedIn = !!user.uid;
+    return (<div className="header">
+      <div className="logo">
+        <h1>
+          <NavLink to='/'>Typist</NavLink>
+        </h1>
       </div>
-    );
+      <div className="menu options">
+        <NavLink className="menu-item" to="/scoreboard">Scoreboard</NavLink>
+      </div>
+      <ul className={cx('menu')}>
+        <MenuItem path="/signup" display={!loggedIn}>Sign up</MenuItem>
+        <MenuItem path="/signin" display={!loggedIn}>Sign In</MenuItem>
+        <MenuItem path="/" display={loggedIn} onClick={() => this.signout()}>Logout</MenuItem>
+        <MenuItem path="/profile" display={loggedIn} title={user.displayName || user.email}>
+          <Avatar user={user} round={true} size="40"/>
+        </MenuItem>
+      </ul>
+    </div>);
   }
 }
