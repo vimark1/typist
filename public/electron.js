013f7569866fe434944eb22dad91216846657702
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2018-10-31 08:18:26 -0600

    style: 💄 fix source code format
    Use prettier to format the code

--- public/electron.js
+++ public/electron.js
@@ -3,15 +3,18 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path');
-const url = require('url');
 const isDev = process.env.NODE_ENV !== 'production' && !app.isPackaged;
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
-  mainWindow.on('closed', () => mainWindow = null);
+  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+  mainWindow.on('closed', () => (mainWindow = null));
 }
 
 app.on('ready', createWindow);
@@ -24,6 +27,6 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
   if (mainWindow === null) {
-createWindow();
+    createWindow();
   }
 });
