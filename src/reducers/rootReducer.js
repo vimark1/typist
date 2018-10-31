71daee94cf54e44f5c50033f7d408835e93ddab6
Clay Jensen-Reimann <clayreimann@gmail.com>
2018-10-30 20:04:51 -0500

    Remove useless reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,9 +1,7 @@
 import {combineReducers} from 'redux';
 import userPreferencesReducer from './userPreferences';
 
-const sampleReducer = (state = null, action) => state;
 const rootReducer = combineReducers({
-  state: sampleReducer,
   userPreferences: userPreferencesReducer,
 });
 
