9d6cdef36e889e7505629cad273f571c4ded86bc
vimark1 <vimark1@users.noreply.github.com>
2018-10-31 20:12:20 +1300

    Avatar component using typescript
    squash! Avatar component using typescript

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Avatar from 'react-avatar';
+
+import { User } from '../../models';
+
+export default function({ user, size = '40', round = true}: { user: User, size: string, round: boolean }) {
+
+  const getGoogleId = () => {
+    const { providerData } = user;
+    const googleProvider = providerData.find(item => item.providerId === 'google.com');
+    if (googleProvider) {
+      return googleProvider.uid;
+    }
+  }
+
+  const googleId: string | undefined = getGoogleId();
+
+  return (
+    <Avatar googleId={googleId} name={user.displayName} email={user.email} round={round} size={size} />
+  );
+
+}
+
