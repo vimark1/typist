4413b646db92a96ce1390d280889d0e9d5c88e83
vimark1 <vimark1@users.noreply.github.com>
2020-06-27 11:25:30 +1200

    prettier

--- src/LineChartStat/index.js
+++ src/LineChartStat/index.js
@@ -1,55 +1,48 @@
 import React from 'react';
 import ReactGA from 'react-ga';
-import firebase from 'firebase/app'
+import firebase from 'firebase/app';
 import LineChart from 'react-linechart';
 import '../../node_modules/react-linechart/dist/styles.css';
 
 class LineChartStat extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            top10 : [
-                //{user: (user object), score: "12"}
-            ]
-        }
-    }
-    
-    componentDidMount() {
-        ReactGA.pageview('/chart');
-          const topScorersRef = firebase.database().ref('top-scorers');
-          topScorersRef.on('value', snapshot => {
-              this.setState({
-                  top10: snapshot.val()
-              })
-          })
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      top10: [
+        //{user: (user object), score: "12"}
+      ],
+    };
+  }
 
-    render() {
-        let tableData = this.state.top10.map((row, idx) => {
-            return {x: idx+1, y: row.score};
-        });
-        const data = [
-            {									
-                color: "steelblue", 
-                points: tableData,
-                
-            },
-            
-            
-        ];
-        return (
-            <div>
-                <div className="App">
-                    <h1>My First LineChart</h1>
-                    <LineChart 
-                        width={600}
-                        height={400}
-                        data={data}
-                    />
-                </div>				
-            </div>
-        );
-    }
+  componentDidMount() {
+    ReactGA.pageview('/chart');
+    const topScorersRef = firebase.database().ref('top-scorers');
+    topScorersRef.on('value', snapshot => {
+      this.setState({
+        top10: snapshot.val(),
+      });
+    });
+  }
+
+  render() {
+    let tableData = this.state.top10.map((row, idx) => {
+      return { x: idx + 1, y: row.score };
+    });
+    const data = [
+      {
+        color: 'steelblue',
+        points: tableData,
+      },
+    ];
+    return (
+      <div>
+        <div className="App">
+          <h1>My First LineChart</h1>
+          <LineChart width={600} height={400} data={data} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LineChartStat;
